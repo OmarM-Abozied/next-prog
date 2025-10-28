23de869ffde7337f20f5bfc919e7bba8d1ba8ed5
omar <@email.com>
2025-10-28 21:07:47 +0300

    newcommit

--- app/todos/page.js
+++ app/todos/page.js
@@ -1,17 +1,16 @@
-export const dynamic = "force-dynamic";
-
+import { deleteTodo } from "@/lib/action";
+import { GET } from "../api/todos/route";
 import Button from "@/compnants/Button";
 
+export const dynamic = "force-dynamic";
 async function TodosList() {
-  const res = await fetch("http://localhost:3000/api/todos", {
-    cache: "no-store",
-  });
+  const res = await GET();
   const data = await res.json();
 
   return (
     <div className="p-6 bg-gray-100 min-h-screen">
       <h1 className="text-3xl font-bold mb-6 text-center text-gray-800">
-        Todos Lis
+        Todos List
       </h1>
       <div className="space-y-4">
         {data.map((todo) => (
